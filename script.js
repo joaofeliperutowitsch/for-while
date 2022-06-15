8b24efdabc2794dca0dcefaed3dd9ba289df3241
joaofeliperutowitsch <jfrutowitsch@gmail.com>
2022-06-14 22:50:10 -0300

    Enabling javascript in html. and not providing javascript 1-Create a loop that counts all numbers from 1 to 10 using FOR, 2-Create a loop that counts from 10 to 1 using WHILE, 3-Create a loop that counts all odd numbers from 1 100, 4- Create a loop that counts all even numbers from 0 to 100

---
+++ script.js
@@ -0,0 +1,19 @@
+for (let count1 = 1; count1 <= 10; count1 += 1){
+    console.log(count1)
+} 
+
+let count2 = 10
+while(count2 >= 1){
+    console.log(count2)
+    count2 -=1
+}
+
+let count3 = 1
+while(count3 <= 100){
+    console.log(count3)
+    count3 +=2
+}
+
+for (let count4 = 0; count4 <= 100; count4 +=2){
+    console.log(count4)
+}
